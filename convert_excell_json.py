4d6eef8d8ce53a2b503e90b667cc1f03e9eb839d
Nindy1 <nindyaghinaramadhani@gmail.com>
2025-07-15 13:50:28 +0700

    Update convert_excell_json.py

--- convert_excell_json.py
+++ convert_excell_json.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-# Baca file Excel
 df = pd.read_excel('LIST BAHAN KIMIA ORGANIK LPK.xlsx')
-
-# Konversi ke JSON
 df.to_json('LIST BAHAN KIMIA ORGANIK LPK.json', orient='records', force_ascii=False, indent=2)
-
-print("Konversi selesai! File JSON disimpan.")
 
